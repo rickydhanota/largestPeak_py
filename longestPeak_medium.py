56ec59974fbb94aaaee591e5280e3c33f9710f93
Ricky <rickdhanota@gmail.com>
2020-09-10 23:53:24 -0700

    added functionality

--- longestPeak_medium.py
+++ longestPeak_medium.py
@@ -2,3 +2,29 @@
 # For example, the integers 1, 4, 10, 2 for a peak, but the integers 4, 0, 10 dont and neither do the integers 1, 2, 2, 0. Similarily, the integers 1,2,3 dont form a peak because there arent any strictly decreasing integers after the 3.
 
 # Example input array = [1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]
+
+def longestPeak(array):
+    longestPeakLength = 0
+    i = 1
+    while i <len(array)-1:
+        isPeak = array[i-1] < array[i] and array[i] > array[i+1]
+        if not isPeak:
+            i += 1
+            continue
+
+        leftIdx = i -2
+        while leftIdx >= 0 and array[leftIdx] < array[leftIdx + 1]:
+            leftIdx -= 1
+        
+        rightIdx = i+2
+        while rightIdx <len(array) and array[rightIdx] < array[rightIdx - 1]:
+            rightIdx += 1
+        
+        currentPeakLength = rightIdx - leftIdx -1
+        longestPeakLength = max(longestPeakLength, currentPeakLength)
+        i = rightIdx
+    return longestPeakLength
+
+
+print(longestPeak([1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]))
+# O(n) time | O(1) space - where n is the length of the input array
